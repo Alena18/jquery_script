23ac2501318d00c5e07dc7efe551804c073cdc6a
Alena18 <sun180983@gmail.com>
2023-02-16 13:45:13 +0000

    Manipulate with odd and even selector

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,22 @@
+// $(document).ready(function() {
+//     $("h2").css("text-decoration", "underline"); //underliners all <h2> elements
+//     $("ul").css("border","solid 1px #ccc"); // adds a border to all lists
+
+
+// });
 $(document).ready(function() {
-    $("h2").css("text-decoration", "underline"); //underliners all <h2> elements
+    $("h2").addClass("underline"); //underliners all <h2> elements
+    $("ul").addClass("border"); // adds a border to all lists
     
+
 });
+
+// $(document).ready(function() {
+//     $("h2").removeClass("underline"); //underliners all <h2> elements
+//     $("ul").removeClass("border"); // adds a border to all lists
+    
+
+// });
+
+// $("tr:odd").addClass("odd"); //add class with odd selector (every second row in a table)
+// $("tr:even").addClass("even"); //add class with even selector
